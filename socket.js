cef6a2615bc199b77e95fe42df3f38a520621f93
andrewfayres <afayres@us.ibm.com>
2016-07-06 11:35:57 -0500

    Added automatic reconnect for twitter api with backoff

--- socket.js
+++ socket.js
@@ -17,6 +17,7 @@
 'use strict';
 
 var debug = require('debug')('scc:socket');
+var backoff = require('backoff');
 var processTweet = require('./util/process-tweet');
 var topic = process.env.TWITTER_TOPIC || '<topic>';
 var preloadedTweets = [];
@@ -25,27 +26,35 @@ module.exports = function(io, twitter) {
   // existing sessions
   var sessions = {};
 
-  twitter.stream({ track: topic, filter_level: 'low', language: 'en' }, function(stream) {
-    debug('Connected to twitter.stream, topic: %s', topic);
-    stream.on('data', function(tweet) {
-      processTweet(tweet, function (error, processedTweet) {
-        if (error) {
-          debug('Ignore tweet: %s', tweet.text);
-          return;
-        }
-        preloadedTweets.push(processedTweet);
-        if(preloadedTweets.length > 5) {
-          preloadedTweets.shift();
-        }
-        Object.keys(sessions).forEach(function(id) {
-          sessions[id].socket.emit('message', processedTweet);
-        });
-      })
+  var connectTwitter = function() {
+    twitter.stream({ track: topic, filter_level: 'low', language: 'en' }, function(stream) {
+      debug('Connected to twitter.stream, topic: %s', topic);
+      stream.on('data', function(tweet) {
+        processTweet(tweet, function (error, processedTweet) {
+          if (error) {
+            debug('Ignore tweet: %s', tweet.text);
+            return;
+          }
+          preloadedTweets.push(processedTweet);
+          if(preloadedTweets.length > 5) {
+            preloadedTweets.shift();
+          }
+          Object.keys(sessions).forEach(function(id) {
+            sessions[id].socket.emit('message', processedTweet);
+          });
+        })
+      });
+      stream.on('error', function(error) {
+        debug('Error connecting to twitter.stream: %s', error);
+        fibonacciBackoff.backoff();
+      });
+
+      stream.on('close', function(error) {
+        debug('Error connecting to twitter.stream: %s', error);
+        fibonacciBackoff.backoff();
+      });
     });
-    stream.on('error', function(error) {
-      debug('Error connecting to twitter.stream: %s', error);
-    });
-  });
+  }
 
   // Create a session on socket connection
   io.use(function(socket, next) {
@@ -70,4 +79,29 @@ module.exports = function(io, twitter) {
       debug('Disconnect: %s', socket.id);
     });
   });
+
+
+  var fibonacciBackoff = backoff.fibonacci({
+    randomisationFactor: 0,
+    initialDelay: 10,
+    maxDelay: 30000
+  });
+
+  fibonacciBackoff.failAfter(10);
+
+  fibonacciBackoff.on('backoff', function(number, delay) {
+      // Do something when backoff starts, e.g. show to the
+      // user the delay before next reconnection attempt.
+      console.log('Attempting to reconect to twitter. Attempts: ' + number + ' ' + delay + 'ms');
+  });
+
+  fibonacciBackoff.on('ready', function() {
+    connectTwitter();
+  })
+
+  fibonacciBackoff.on('fail', function() {
+    debug('Failed to connect to twitter after multiple attempts.')
+  })
+
+  fibonacciBackoff.backoff();
 };
